205ee8161812c93f0b40c5f8b899d99960170459
52646 <52646@etu.he2b.be>
2021-02-20 17:41:28 +0100

    Update board.h

--- core/board.h
+++ core/board.h
@@ -4,8 +4,55 @@
 
 class board
 {
+private:
+
+/   *!
+     * \ .
+     */
+    ;
+
+    /*!
+     * \brief Attribute representing the new position.
+     */
+    Position newPosition;
+
+    /*!
+     * \ .
+     */
+    ;
+    /*!
+     * \ .
+     */
+    ;
+
+
 public:
+      /*!
+     * \brief Constructor of the board class.
+     */
     board();
+
+    /*!
+     * \brief method  announces the position the marble on the board.
+     * \param String announces the position.
+     */
+    void calculePosition(String);
+
+    /*!
+     * \brief method that add the marble in the board
+     */
+    void addMarble();
+
+    /*!
+     * \brief method that remove the marble in the board
+     */
+    void removeMarble();
+
+    /*!
+     * \brief method that tells if the move is allowed or not.
+     */
+    void legalMove();
+
 };
 
 #endif // BOARD_H
