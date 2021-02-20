7578c33979d590c84c7860fca35545bc4fbfbab0
christopher <christopher.bourrel0429@gmail.com>
2021-02-20 15:56:29 +0100

    Header + documentation for the square + position

--- core/square.h
+++ core/square.h
@@ -1,11 +1,55 @@
 #ifndef SQUARE_H
 #define SQUARE_H
 
+#include "color.h"
+#include "position.h"
 
 class square
 {
+private:
+    /*!
+     * \brief Attribute that tell is the square are a marble or not.
+     */
+    bool m_isEmpty;
+    /*!
+     * \brief Attribute representing the position (x,y,z) for the square.
+     */
+    Position m_pos;
+    /*!
+     * \brief Attribute representing the color of the marble.
+     */
+    Color color;
+
 public:
+    /*!
+     * \brief Constructor of the square class.
+     */
     square();
+    /*!
+     * \brief Constructor of the square class with attribute (true for the isEmpty).
+     * \param m_pos the position of the square
+     * \param m_color the color of the square
+     */
+    square(Position m_pos, Color m_color);
+    /*!
+     * \brief Getter of the attribute Color.
+     */
+    Color getColor();
+    /*!
+     * \brief Getter of the attribute isEmpty.
+     * \return is the square is empty or not.
+     */
+    bool getIsEmpty();
+
+    /*!
+     * \brief Getter of the attribute isEmpty.
+     * \return Class Position.
+     */
+    Position getPosition();
+    /*!
+     * \brief Change the position of the square is a marble is moved.
+     */
+    void changePosition(int x, int y, int z);
 };
 
 #endif // SQUARE_H
